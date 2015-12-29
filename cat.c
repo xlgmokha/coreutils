fb66faeccd557d90bafcff5e951ccf37b13499ca
mo khan <mo@mokhan.ca>
2015-12-28 21:41:23 -0700

    add support for cat -n.

--- cat.c
+++ cat.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-  char *program_name = argv[0];
-  char *filename = argv[1];
+  int choice;
+  int show_lines = 0;
+
+  while ( (choice = getopt(argc, argv, "n")) != -1 ) {
+    switch(choice) {
+      case 'n':
+        show_lines = 1;
+        break;
+      case '?':
+      default:
+        break;
+    }
+  }
+  argc -= optind;
+  argv += optind;
+
+  char *filename = argv[0];
   FILE *file = fopen(filename, "r");
 
   if (file) {
-    int character;
+    int character, previous;
+    int line = 0;
+
+    if (show_lines == 1) {
+      printf("%6d\t", ++line);
+    }
+
     while( (character = getc(file)) != EOF ) {
+      if (previous == '\n' && show_lines == 1) {
+        printf("%6d\t", ++line);
+      }
       putchar(character);
+      previous = character;
     }
     fclose(file);
   }
